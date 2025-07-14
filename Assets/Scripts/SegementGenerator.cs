d8865e90d256f7ce29f53a59f665e4522929566c
Shreyas <130305456+shreyas129@users.noreply.github.com>
2025-07-14 23:32:27 +0530

    Half change but required because its a part of it resolving next time

--- Assets/Scripts/SegementGenerator.cs
+++ Assets/Scripts/SegementGenerator.cs
@@ -3,13 +3,17 @@ using UnityEngine;
 
 public class SegementGenerator : MonoBehaviour
 {
-    public GameObject segementMap01;
-    public GameObject segementMap02;
-    public GameObject segementMap03;
-    public GameObject segementMap04;
-    public GameObject segementMap05;
-    public GameObject segementMap06;
-    public GameObject segementMap07;
+    public GameObject segmentMap01;
+    public GameObject segmentMap02;
+    public GameObject segmentMap03;
+    public GameObject segmentMap04;
+    public GameObject segmentMap05;
+    public GameObject segmentMap06;
+    public GameObject segmentMap07;
+
+    [SerializeField] int zPos = 50;
+    [SerializeField] bool creatingSegment = false;
+    [SerializeField] int segmentNum = 0;
 
     void Start()
     {

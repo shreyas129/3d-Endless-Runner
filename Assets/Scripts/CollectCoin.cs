61ee630013e0ba99791491dd75086f5c6b41229d
Shreyas <130305456+shreyas129@users.noreply.github.com>
2025-07-20 19:05:02 +0530

    I added  some sounds
    needed to be correctly triggered but the coin sound is almost done

---
+++ Assets/Scripts/CollectCoin.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class CollectCoin : MonoBehaviour
+{
+    [SerializeField] AudioSource coinFX;
+
+    void OnTriggerEnter(Collider other)
+    {
+        coinFX.Play();
+        this.gameObject.SetActive(false);
+    }
+}

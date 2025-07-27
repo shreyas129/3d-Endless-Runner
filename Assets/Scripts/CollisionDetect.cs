57f0087170fb7f97d9617f359fe71db8620d9b90
Shreyas <130305456+shreyas129@users.noreply.github.com>
2025-07-27 22:52:36 +0530

    Add new coin GameObjects to SampleScene
    Added several new Coin GameObjects (Coin 15, 16, 17, 18, 24) to the SampleScene.unity file, each with associated components for rendering, collision, and behavior. Also updated some object positions and replaced a Rock01 GameObject with a Coin. Adjusted prefab instance placements for Plant_3 and Terrain_1 (4) to maintain scene consistency.

---
+++ Assets/Scripts/CollisionDetect.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class CollisionDetect : MonoBehaviour
+{
+//    [SerailizeFeild]
+//    void OnTriggerEnter(Collider other)
+//    {
+//    coinFX.Play();
+//    MasterInfo.coinCount += 1;
+//        this.gameObject.SetActive(false);
+//    }
+}

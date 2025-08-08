1793dae4fc00c1355df040892947f808ef5f0c83
Shreyas <130305456+shreyas129@users.noreply.github.com>
2025-08-08 23:44:08 +0530

    Add 'Stumble Backwards' animation to Timmy
    Introduced a new 'Stumble Backwards' animation and integrated it into Timmy's animator controller. Updated related scripts and the SampleScene to support the new animation state.

--- Assets/Scripts/CollisionDetect.cs
+++ Assets/Scripts/CollisionDetect.cs
@@ -3,8 +3,10 @@ using UnityEngine;
 public class CollisionDetect : MonoBehaviour
 {
     [SerializeField] GameObject thePlayer;
+    [SerializeField] GameObject playerAnim;
     void OnTriggerEnter(Collider other)
     {
         thePlayer.GetComponent<PlayerMovement>().enabled = false;
+        playerAnim.GetComponent<Animator>().Play("Stumble Backwards");
     }
 }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,8 +4,8 @@ public class PlayerMovement : MonoBehaviour
 {
     public float playerSpeed = 6;
     public float horizontalSpeed = 3;
-    public float rightLimit = 5.5f;
-    public float leftLimit = 5.5f;
+    public float rightLimit = 6.5f;
+    public float leftLimit = -6.5f;
 
     void Update()
     {

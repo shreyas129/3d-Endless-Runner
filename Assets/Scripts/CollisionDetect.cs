dad72c9f24bb9d4ffdb38479af4cc1895fffbe51
Shreyas <130305456+shreyas129@users.noreply.github.com>
2025-08-09 23:48:01 +0530

    Add collision animation, sound, and fade out effect
    Introduced new animation assets for collision and fade out effects, added a collision sound effect, and updated the SampleScene to reference these new assets. Modified CollisionDetect.cs to support triggering the collision FX, camera animation, and fade out. Adjusted object positions in the scene for improved layout.

--- Assets/Scripts/CollisionDetect.cs
+++ Assets/Scripts/CollisionDetect.cs
@@ -1,12 +1,26 @@
+using System.Collections;
 using UnityEngine;
 
 public class CollisionDetect : MonoBehaviour
 {
     [SerializeField] GameObject thePlayer;
     [SerializeField] GameObject playerAnim;
+    [SerializeField] AudioSource collisionFX;
+    [SerializeField] GameObject mainCam;
+    [SerializeField] GameObject fadeOut;
+
     void OnTriggerEnter(Collider other)
     {
+        StartCoroutine(CollisionEnd());
+    }
+
+    IEnumerator CollisionEnd()
+    {
+        collisionFX.Play();
         thePlayer.GetComponent<PlayerMovement>().enabled = false;
         playerAnim.GetComponent<Animator>().Play("Stumble Backwards");
+        mainCam.GetComponent<Animator>().Play("CollisionCam");
+        yield return new WaitForSeconds(3);
+        fadeOut.SetActive(true);
     }
 }

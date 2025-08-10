57fb3b832b59d991b538ddc426402ad0cf880d29
Shreyas <130305456+shreyas129@users.noreply.github.com>
2025-08-10 20:53:37 +0530

    Add main menu scene and integrate scene management
    Renamed SampleScene to DesertRun and added a new MainMenu scene with UI and control script. Updated CollisionDetect to reload the main menu on collision, created MainMenuControl for starting the game, and updated EditorBuildSettings to include both scenes in the build order.

--- Assets/Scripts/CollisionDetect.cs
+++ Assets/Scripts/CollisionDetect.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CollisionDetect : MonoBehaviour
 {
@@ -22,5 +23,7 @@ public class CollisionDetect : MonoBehaviour
         mainCam.GetComponent<Animator>().Play("CollisionCam");
         yield return new WaitForSeconds(3);
         fadeOut.SetActive(true);
+        yield return new WaitForSeconds(3);
+        SceneManager.LoadScene(0);
     }
 }
---
+++ Assets/Scripts/MainMenuControl.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuControl : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
